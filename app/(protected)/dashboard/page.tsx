05f8ce77a1ce415ebba6a49fb90b9427edbd41b3
santiago souto <souto.santiago@academico.ifpb.edu.br>
2025-07-27 18:51:09 -0300

    dashboard

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,8 +1,7 @@
 import { LogoutButton } from '@/components/logout-button'
 import { createClient } from '@/lib/server'
 
-export default async function ProtectedPage() {
-  
+export default async function Dashboard() {
   return (
     <div className="flex h-svh w-full items-center justify-center gap-2">
       <p>
